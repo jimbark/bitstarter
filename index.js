4acce3eaf6f45107bbac5ff5e55e6377d7e76dcd
Jim Bark <jimbark007@gmail.com>
2014-06-22 13:56:50 +0000

    edited to state dev version

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 var data = fs.readFileSync('index.html');
 var data1 = data.toString();
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 8080));
 app.use(express.static(__dirname + '/public'));
 
 
